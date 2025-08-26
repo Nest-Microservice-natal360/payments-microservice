8ce8b7956655d00dea3adfebc36e41a1db861930
natal360 <drossel.fb@gmail.com>
2025-08-26 14:23:47 +0900

    update submodule orders-ms and payment-ms

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,12 +1,18 @@
 import Stripe from 'stripe';
-import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { envs, NATS_SERVICE } from 'src/config';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymentsService');
+
+  constructor(
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy,
+  ){}
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
     const { currency, items, orderId } = paymentSessionDto;
@@ -30,7 +36,7 @@ export class PaymentsService {
     const session = await this.stripe.checkout.sessions.create({
       payment_intent_data: {
         metadata: {
-          orderId: orderId
+          orderId: orderId,
         },
       },
 
@@ -40,37 +46,50 @@ export class PaymentsService {
       cancel_url: envs.stripeCancelUrl,
     });
 
-    return session;
+    // return session
+    return {
+      cancelUrl: session.cancel_url,
+      successUrl: session.success_url,
+      url: session.url,
+    };
   }
 
-  async stripeWebhook(req: Request, res: Response){
+  async stripeWebhook(req: Request, res: Response) {
     const sig = req.headers['stripe-signature'];
 
-    let event : Stripe.Event
+    let event: Stripe.Event;
     // Real
     const endpointSecret = envs.stripeEndpointSecret;
 
     try {
-      event = this.stripe.webhooks.constructEvent(req['rawBody'], sig, endpointSecret);
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
     } catch (err) {
-       res.status(400).send(`Webhook Err: ${err.message}`);
-    return;
+      res.status(400).send(`Webhook Err: ${err.message}`);
+      return;
     }
 
-    switch(event.type){
-      case 'charge.succeeded' :
+    switch (event.type) {
+      case 'charge.succeeded':
         const chargeSucceeded = event.data.object;
-        // TODO
-        console.log({
-          metadata: chargeSucceeded.metadata,
-          orderId: chargeSucceeded.metadata.orderId
-        });
-      break;
+        const payload = {
+          stripePaymentId: chargeSucceeded.id,
+          orderId: chargeSucceeded.metadata.orderId,
+          receiptUrl: chargeSucceeded.receipt_url,
+        };
+
+        // this.logger.log({ payload });
+        this.client.emit('payment.succeeded', payload);
+
+        break;
 
       default:
-        console.log(`Event ${event.type} not handled`)
+        console.log(`Event ${event.type} not handled`);
     }
 
-    return res.status(200).json({sig})
+    return res.status(200).json({ sig });
   }
 }
